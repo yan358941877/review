ad832c27a4e506410c68e711212e442fcbcb9740
yan358941877 <yan358941877@163.com>
2017-08-24 15:07:01 +0800

    发布订阅模式

---
+++ DesignPattern/subscribe-publish.js
@@ -0,0 +1,24 @@
+var EventCenter = (function(){
+  var listeners = {}
+
+  // 注册事件
+  function on(event, handler){
+    listeners[event] = listeners[event] || []
+    listeners[event].push({
+      handler
+    })
+  }
+
+  // 发布事件
+  function fire(event, args){
+    if(!listeners[event]){
+      return 
+    }
+    for(var i in listeners[event]){
+      listeners[event][i].handler(args)
+    }
+  }
+  return {
+    on, fire
+  }
+})()
